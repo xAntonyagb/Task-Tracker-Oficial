d58a82301849a3ecc1fa7cefe083f7308757045f
Antony <antonyagb.pv@gmail.com>
2024-10-22 22:02:55 -0300

    Add componente usuarios lateral

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -10,3 +10,40 @@ import { Component } from '@angular/core';
 export class UserComponent {
 
 }
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Maria Silva',
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: 'u2',
+    name: 'Catarina Morgado',
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'Marcos Paulo',
+    avatar: 'user-3.jpg',
+  },
+  {
+    id: 'u4',
+    name: 'José Moura',
+    avatar: 'user-4.jpg',
+  },
+  {
+    id: 'u5',
+    name: 'Carla Souza',
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: 'u6',
+    name: 'Arthur Vale',
+    avatar: 'user-6.jpg',
+  },
+  {
+    id: 'u7',
+    name: 'Pedro Careca',
+    avatar: 'user-1.jpg',
+  }
+];
